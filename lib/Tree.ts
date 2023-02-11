e9dc60138a9ade2ab22a5932bb3f5f6c9f75d932
Emre Uğur <46279069+Emruur@users.noreply.github.com>
2023-02-11 16:58:00 +0300

    Remove existing files

--- lib/Tree.ts
+++
@@ -1,7 +0,0 @@
-export default interface Tree<TreeType>
-{
-    add(data: TreeType): void
-    remove(data: TreeType): void
-    drawTree(ctx: CanvasRenderingContext2D, width: number): void
-    clear():void
-}
